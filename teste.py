399ed6ec6000c9b25c13c0df9c3da68fb93da213
kalyan <kalyan@unochapeco.edu.br>
2022-12-07 22:02:08 -0300

    ultima atualizacao

--- teste.py
+++ teste.py
@@ -10,17 +10,19 @@ captura = cv.VideoCapture("./video/video-chao.mp4")
 
 while(1):
     ret, frame = captura.read()
-    # sleep(1)
+    
     frame = cv.flip(frame, 0) 
     # find red retangle
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    lower_red = np.array([0,50,50])
-    upper_red = np.array([10,255,255])
+    
+    lower_red = np.array([50,50,50])
+    upper_red = np.array([200,200,200])
+    
     mask = cv.inRange(hsv, lower_red, upper_red)
     contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         area = cv.contourArea(cnt)
-        if area > 30:
+        if area > 3000:
             cv.drawContours(frame, [cnt], 0, (0, 255, 0), 3)
             M = cv.moments(cnt)
             cx = int(M['m10']/M['m00'])
@@ -30,18 +32,10 @@ while(1):
             cv.putText(frame,'Centro',(cx-20,cy-20), font, 0.5,(255,255,255),2,cv.LINE_AA)
     cv.imshow('frame',hsv)
     cv.imshow('mask',mask)
-    # k = cv.waitKey(5) & 0xFF
-    # if k == 27:
-    #     break
-    
-    # edges = cv.Canny(frame, threshold1=30, threshold2=100)
-    # cv.imshow("Video", frame)
-    
-    
+    cv.imshow('frame2',frame)
    
     k = cv.waitKey(30) & 0xff
     if k == 'ESC':
-        print("macacoooooooo")
         break
 
 captura.release()       
